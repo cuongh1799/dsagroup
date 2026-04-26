3e0929084d9815dd7263f999085987146b66f476
cuongh1799 <cuonghuynh1009@gmail.com>
2026-04-26 23:04:14 +0700

    refactor map generation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Map Routing System with File Export
+# Main Application
 # Developed in a Java-style architecture
 
 import random
@@ -7,38 +7,48 @@ from exporter import FileExporter
 from pathSystem import RoutingEngine
 
 def main():
+    TOTAL_NODES = 100 
+    
     myMap = MapGraph()
-    # Generate 2000 nodes
-    for i in range(2000):
+    
+    for i in range(TOTAL_NODES):
         myMap.addNode(i)
 
-    # Generate edges
-    for i in range(1999):
-        distance = random.randint(10, 50) # generate random distance 
-        times = [max(1, distance + random.randint(-5, 15)) for _ in range(24)] # populated the time
+
+    for i in range(TOTAL_NODES - 1):
+        distance = random.randint(1, 3) 
+        times = [max(1, distance * 2 + random.randint(1, 5)) for _ in range(24)] 
         myMap.addEdge(i, i + 1, distance, times) 
-        if i % 20 == 0 and i + 100 < 2000:
-            distance2 = random.randint(100, 300)
-            times2 = [max(1, distance2 + random.randint(-10, 40)) for _ in range(24)]
-            myMap.addEdge(i, i + 100, distance2, times2)
+
+        # Create "Shortcuts" (Cross-connections)
+        # We make a shortcut every 10 nodes if the jump doesn't exceed the map size
+        shortcut_interval = 10
+        jump_size = 20
+        if i % shortcut_interval == 0 and (i + jump_size) < TOTAL_NODES:
+            distance2 = random.randint(5, 15)
+            times2 = [max(1, distance2 + random.randint(1, 10)) for _ in range(24)]
+            myMap.addEdge(i, i + jump_size, distance2, times2)
 
     engine = RoutingEngine(myMap)
     exporter = FileExporter()
 
-    # Query
-    source, destination, hour = 0, 1250, 18
-    avoid_nodes = [10, 20]
-    avoid_edges = [(50, 51)]
+    source = 0
+    destination = TOTAL_NODES - 1 # Set destination to the last node
+    hour = 18 # 6:00 PM
+    # avoid_nodes = [10, 20]
+    # avoid_edges = [(50, 51)]
 
-    # Run Algorithms
-    resDist = engine.findPath(source, destination, avoid_nodes, avoid_edges, "DISTANCE", hour)
-    resTime = engine.findPath(source, destination, avoid_nodes, avoid_edges, "TIME", hour)
+    # Run Dijkstra for both Distance and Time
+    # (Passing None for avoid_nodes and avoid_edges as requested)
+    resDist = engine.findPath(source, destination, None, None, "DISTANCE", hour)
+    resTime = engine.findPath(source, destination, None, None, "TIME", hour)
 
-    # Export to Files
+    # --- EXPORTING ---
     exporter.exportGraph(myMap, "map_data.txt")
     exporter.exportRouteResults(resDist, resTime, "route_results.txt", source, destination, hour)
 
-    print("\nProcesses finished. Check 'map_data.txt' and 'route_results.txt'.")
+    print("\nProcesses finished successfully.")
+    print("Files created: 'map_data.txt' and 'route_results.txt'")
 
 if __name__ == "__main__":
     main()
